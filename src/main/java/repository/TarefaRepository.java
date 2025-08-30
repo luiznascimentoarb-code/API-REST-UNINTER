3d61d990533140e13947ee9a6c99a901ff42019b
luiznascimentoarb-code <luiznascimentoarb@gmail.com>
2025-08-30 18:34:49 -0300

    Delete src directory

--- src/main/java/repository/TarefaRepository.java
+++
@@ -1,6 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import model.Tarefa;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {}
